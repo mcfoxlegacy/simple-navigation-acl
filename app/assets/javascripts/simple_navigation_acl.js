0206a0201791820dc9d718c654a39c4f6d1e4040
Bruno Porto <brunotporto@gmail.com>
2016-11-23 13:29:27 -0200

    codigo jquery para auxiliar na seleção dos campos.

--- app/assets/javascripts/simple_navigation_acl.js
+++ app/assets/javascripts/simple_navigation_acl.js
@@ -1,13 +1,15 @@
 $(document).on('change','.acl_tree_checkbox', function(ev) {
     var checkbox = $(this);
-
     if (checkbox.prop('checked')==true) {
         var acl_tree_parents = checkbox.parents('.acl_tree_item');
         acl_tree_parents.find('> :checkbox').prop('checked', true);
     } else {
         var acl_tree_item = checkbox.parent('.acl_tree_item:first');
-        console.log(acl_tree_item);
         acl_tree_item.find(':checkbox').prop('checked', false);
+        // REMOVER O PRIMEIRO PAI QUANDO OS FILHOS ESTIVERM TODOS DESMARCADOS
+        // var acl_tree_parent = acl_tree_item.parents('.acl_tree_item:first');
+        // if (acl_tree_parent.find('.acl_tree_item > :checkbox:checked').length==0) {
+        //     acl_tree_parent.find(':checkbox:first').prop('checked', false);
+        // }
     }
-
 });
